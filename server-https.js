ef0698bc4ed2c1465effbf46cbd0efcab1a32014
Daniel Holmlund <agnathan@gmail.com>
2017-05-12 06:09:44 -0700

    Replacing certs
    The other lab-solutions repositories all have the same certs checked
    in. Move to those certs.

---
+++ server-https.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+    https = require('https'),
+    fs = require('fs');
+
+var PORT = process.env.PORT || 8443;
+var HOST = process.env.HOST || '';
+
+var app = express();
+
+app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.static('public'));
+    app.use(app.router);
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+	app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res) {
+    res.end('Welcome on the secure server !');
+});
+
+var options = {
+    key  : fs.readFileSync('certs/server.key'),
+    ca   : fs.readFileSync('certs/server.csr'),
+    cert : fs.readFileSync('cert/server.crt')
+}
+
+https.createServer(options, app).listen(PORT, HOST, null, function() {
+    console.log('Server listening on port %d in %s mode', this.address().port, app.settings.env);
+});
